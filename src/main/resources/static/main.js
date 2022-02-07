b31f09033a86535a8cc0e2ef578b235cdcde57a7
Mikhail <mbesurich@mail.ru>
2022-02-07 20:59:40 +0300

    вот теперь работает на js: printUsers и createNewUserForm

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,28 +1,77 @@
-let allUsersForm = document.getElementById('listOfUsers');
+$(document).ready(printUsers());
+
+
 let createNewUserForm = document.getElementById('createNewUserFormId');
-const url = 'http://localhost:8080/api/users';
-let outputForAllUsers = '<table  class="table table-striped bg-white text-dark">' +
-    '<tr class="bg-white text-dark">' +
-    '<th>ID</th>' +
-    '<th>First Name</th>' +
-    '<th>Last Name</th>' +
-    '<th>Age</th>' +
-    '<th>Email</th>' +
-    '<th>Role</th>' +
-    '<th>Edit</th>' +
-    '<th>Delete</th>' +
-    '</tr>' +
-    '<tbody id="usersTable">';
+let url = 'http://localhost:8080/api/users';
+// let outputForAllUsers = '<table  class="table table-striped bg-white text-dark">' +
+//     '<tr class="bg-white text-dark">' +
+//     '<th>ID</th>' +
+//     '<th>First Name</th>' +
+//     '<th>Last Name</th>' +
+//     '<th>Age</th>' +
+//     '<th>Email</th>' +
+//     '<th>Role</th>' +
+//     '<th>Edit</th>' +
+//     '<th>Delete</th>' +
+//     '</tr>' +
+//     '<tbody id="usersTable">';
 
 
-const renderAllUsers = (data) => {
-    data.forEach(element => {
-        let elementRoles = '';
-        element.roleSet.forEach(role => {
-                elementRoles += role.name + ' ';
-            }
-        );
-        outputForAllUsers += `
+// const renderAllUsers = (data) => {
+//     data.forEach(element => {
+//         let elementRoles = '';
+//         element.roleSet.forEach(role => {
+//                 elementRoles += role.name + ' ';
+//             }
+//         );
+//         outputForAllUsers += `
+//             <tr>
+//                 <td> ${element.id} </td>
+//                 <td> ${element.name} </td>
+//                 <td> ${element.surName} </td>
+//                 <td> ${element.age} </td>
+//                 <td> ${element.email} </td>
+//                 <td> ${elementRoles} </td>
+//                 <td>
+//                     <button type="button" class="btn btn-info" data-toggle="modal" id="editModal" attr="data-target='#editModal' onclick="editUser(this)">
+//                         Edit
+//                     </button>
+//                 </td>
+//                 <td>
+//                     <button type="button" class="btn btn-danger" data-toggle="modal" id="deleteModal" attr="data-target='#deleteModal' onclick="deleteRow(this)"">
+//                         Delete
+//                     </button>
+//                 </td>
+//             </tr>`
+//     });
+//     outputForAllUsers += '</tbody>\n' +
+//         '    </table>';
+//     allUsersForm.innerHTML = outputForAllUsers;
+// };
+
+function printUsers() {
+    let url = 'http://localhost:8080/api/users';
+    let allUsersForm = document.getElementById('listOfUsers');
+    let outputForAllUsers = '<table  class="table table-striped bg-white text-dark">' +
+        '<tr class="bg-white text-dark">' +
+        '<th>ID</th>' +
+        '<th>First Name</th>' +
+        '<th>Last Name</th>' +
+        '<th>Age</th>' +
+        '<th>Email</th>' +
+        '<th>Role</th>' +
+        '<th>Edit</th>' +
+        '<th>Delete</th>' +
+        '</tr>' +
+        '<tbody id="usersTable">';
+    const renderAllUsers = (data) => {
+        data.forEach(element => {
+            let elementRoles = '';
+            element.roleSet.forEach(role => {
+                    elementRoles += role.name + ' ';
+                }
+            );
+            outputForAllUsers += `
             <tr>
                 <td> ${element.id} </td>
                 <td> ${element.name} </td>
@@ -31,23 +80,21 @@ const renderAllUsers = (data) => {
                 <td> ${element.email} </td>
                 <td> ${elementRoles} </td>
                 <td>
-                    <button type="button" class="btn btn-info" data-toggle="modal" attr="data-target='#editModal' onclick="editUser(this)">
+                    <button type="button" class="btn btn-info" data-toggle="modal" id="editModal" attr="data-target='#editModal' onclick="editUser(this)">
                         Edit
                     </button>
                 </td>
                 <td>
-                    <button type="button" class="btn btn-danger" data-toggle="modal" attr="data-target='#deleteModal' onclick="deleteRow(this)"">
+                    <button type="button" class="btn btn-danger" data-toggle="modal" id="deleteModal" attr="data-target='#deleteModal' onclick="deleteRow(this)"">
                         Delete
                     </button>
                 </td>
             </tr>`
-    });
-    outputForAllUsers += '</tbody>\n' +
-        '    </table>';
-    allUsersForm.innerHTML = outputForAllUsers;
-};
-
-function printUsers() {
+        });
+        outputForAllUsers += '</tbody>\n' +
+            '    </table>';
+        allUsersForm.innerHTML = outputForAllUsers;
+    };
     fetch(url)
         .then(res => res.json())
         .then(data => renderAllUsers(data));
@@ -96,32 +143,37 @@ createNewUserForm.addEventListener('submit', (e) => {
         });
 });
 
-function editUser(o) {
-    document.getElementById('editForm').reset();
-    let id = $(o).closest('tr').find('td').eq(0).text();
-    fetch(url + id)
-        .then((response) => {
-            response.json().then((data) => {
-                $('#idEdit').val(data.id);
-                $('#firstNameEdit').val(data.name);
-                $('#lastNameEdit').val(data.surName);
-                $('#AgeEdit').val(data.age);
-                $('#emailEdit').val(data.email);
-                $('#passwordEdit').val(data.password);
 
-                var roles = data.roles;
-                var newRoles = [];
-                $('#EditCheckRoles option').each(function () {
-                    newRoles[$(this).val()] = $(this).val();
-                });
-                roles.forEach(function (item) {
-                    if (newRoles.includes(String(item.id))) {
-                        $('#editRoles option[id=' + item.id + ']').prop('selected', true);
-                    }
-                })
-            });
-        });
-};
+allUsersForm.addEventListener('click', (e) => {
+    console.log(e.target.id);
+})
+
+// function editUser(o) {
+//     document.getElementById('editForm').reset();
+//     let id = $(o).closest('tr').find('td').eq(0).text();
+//     fetch(url + id)
+//         .then((response) => {
+//             response.json().then((data) => {
+//                 $('#idEdit').val(data.id);
+//                 $('#firstNameEdit').val(data.name);
+//                 $('#lastNameEdit').val(data.surName);
+//                 $('#AgeEdit').val(data.age);
+//                 $('#emailEdit').val(data.email);
+//                 $('#passwordEdit').val(data.password);
+//
+//                 var roles = data.roles;
+//                 var newRoles = [];
+//                 $('#EditCheckRoles option').each(function () {
+//                     newRoles[$(this).val()] = $(this).val();
+//                 });
+//                 roles.forEach(function (item) {
+//                     if (newRoles.includes(String(item.id))) {
+//                         $('#editRoles option[id=' + item.id + ']').prop('selected', true);
+//                     }
+//                 })
+//             });
+//         });
+// };
 
 
 
