239076c0055e8975015f6ff67403deea6fdb0a73
Mikhail <mbesurich@mail.ru>
2022-03-02 22:13:53 +0300

    добавил шифрование паролей BCryptPasswordEncoder

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,23 +32,13 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Override
     public void addUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         userDao.addUser(user);
     }
 
     @Override
     public void update(User user) {
-        userDao.update(user);
-    }
-
-    @Override
-    public void update(Long id, String name, String lastName, int age, String email, String password, Set<Role> roles) {
-        User user = userDao.getUserById(id);
-        user.setName(name);
-        user.setSurName(lastName);
-        user.setAge(age);
-        user.setEmail(email);
-        user.setPassword(password);
-        user.setRoleSet(roles);
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         userDao.update(user);
     }
 
